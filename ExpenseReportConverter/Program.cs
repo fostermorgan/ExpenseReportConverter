43b6331658a17699931dec2f8c418fe062a2639f
Foster <foster.morgan11@gmail.com>
2023-11-03 19:09:13 -0500

    Initial commit

---
+++ ExpenseReportConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseReportConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ExpenseReportConverter
+
+https://docs.google.com/document/d/1ekJwvaRrkC_2sBqo-OB1Kz0lFavHwFDPIAahHIH-1_E/edit
+
