011626339623390b7279bc3fd0a2be8affbee640
Foster <foster.morgan11@gmail.com>
2023-11-11 10:01:02 -0600

    Added create excel function back in case i need it

--- ExpenseReportConverter/Program.cs
+++ ExpenseReportConverter/Program.cs
@@ -503,6 +503,71 @@ namespace ExpenseReportConverter
             OutputLine("|__|______|__|");
             OutputLine();
         }
+
+        // Currently not used, but keeping for addt reference
+        public static void CreateExcelFileFromData(List<string> data, string outputFilePath)
+        {
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                // Define the number of rows you want to format
+                int rowsToFormat = Math.Min(2, data.Count);
+
+                // Write the data to the Excel sheet
+                for (int i = 0; i < data.Count; i++)
+                {
+                    worksheet.Cells[i + 1, 1].Value = data[i];
+
+                    if (i < rowsToFormat)
+                    {
+                        // Apply formatting to the first 'rowsToFormat' rows
+                        worksheet.Cells[i + 1, 1].Style.Font.Bold = true;
+                        worksheet.Cells[i + 1, 1].Style.Font.Size = 14;
+                        worksheet.Cells[i + 1, 1].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+                    }
+
+
+                    // Remove the "$" sign and parse the string as a double
+                    if (double.TryParse(data[i].Replace("$", ""), out double numericValue))
+                    {
+                        worksheet.Cells[i + 1, 1].Value = numericValue;
+
+                        // Apply formatting to the cell based on the numeric value
+                        var cell = worksheet.Cells[i + 1, 1];
+                        if (numericValue >= 0)
+                        {
+                            cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                            cell.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGreen);
+                        }
+                        else
+                        {
+                            cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                            cell.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightSalmon);
+                        }
+                    }
+                    else
+                    {
+                        // Handle the case where the value couldn't be parsed as a double
+                        worksheet.Cells[i + 1, 1].Value = data[i];
+                    }
+
+                    //TODO: when searching for the total money amount, scan the column headers for 'Amount' then go down
+                    // also can find the totals easily by finding the intersection of 'Amount' column and 'Total for [expense]' row
+
+                }
+
+                int columnToFormat = 2; // Column B
+                using (var range = worksheet.Cells[1, columnToFormat, data.Count, columnToFormat])
+                {
+                    range.Style.Numberformat.Format = "$#,##0.00";
+                }
+
+                // Save the Excel package to a file
+                package.SaveAs(new FileInfo(outputFilePath));
+            }
+        }
+
     }
 
     public class InputExcelReport
